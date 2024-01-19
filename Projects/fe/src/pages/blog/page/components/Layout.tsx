2347e9d599f0345078f374e6affd4ee20d134080
Jehee Cheon <jeheecheon@gmail.com>
2024-01-20 05:36:15 +0900

    Edited banner

--- Projects/fe/src/pages/blog/page/components/Layout.tsx
+++ Projects/fe/src/pages/blog/page/components/Layout.tsx
@@ -16,10 +16,12 @@ export const Layout = (props: { children: ReactElement | ReactElement[] }) => {
             <Header show={showSidebar} setShowSidebar={setShowSidebar} />
 
             <div className='mb-auto bg-white'>
-                <div className='bg-fixed h-[85vh]' style={{
+                <div className='bg-fixed h-[75vh] bg-center' style={{
                     backgroundImage: `url(${backgroundImage})`
                 }}>
-
+                    <span className='absolute bottom-[20vh] right-4 text-white text-lg'>
+                        Jehee Cheon 😁 항상 화이팅🔥
+                    </span>
                 </div>
                 {props.children}
             </div>
