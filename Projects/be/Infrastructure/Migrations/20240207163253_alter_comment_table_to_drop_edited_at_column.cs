07c2c1fa6d6460c7f197c1dc74a24437a62896a6
Jehee Cheon <jeheecheon@gmail.com>
2024-02-08 01:36:13 +0900

    add migration alter_comment_table_to_drop_edited_at_column

---
+++ Projects/be/Infrastructure/Migrations/20240207163253_alter_comment_table_to_drop_edited_at_column.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    [DbContext(typeof(MainContext))]
+    [Migration("20240207163253_alter_comment_table_to_drop_edited_at_column")]
+    public partial class alter_comment_table_to_drop_edited_at_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+ALTER TABLE comment
+DROP COLUMN edited_at;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+ALTER TABLE comment
+ADD COLUMN edited_at TIMESTAMP WITH TIME ZONE;
+            ");
+        }
+    }
+}
