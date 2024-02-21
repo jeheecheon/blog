29f7e89ce7677f4ddec3ea25263f132320125b6e
Jehee Cheon <jeheecheon@gmail.com>
2024-02-22 04:32:16 +0900

    Changed like button image

--- Projects/fe/src/common/components/post/PostCard.tsx
+++ Projects/fe/src/common/components/post/PostCard.tsx
@@ -26,13 +26,18 @@ const PostCard: React.FC<PostCardProps> = ({ className, post }) => {
             group-hover:text-sky-700 dark:group-hover:text-default-7'>
                 {post.Title}
             </div>
-            
+
             <div className='flex flex-row justify-start items-end fill-sky-700 dark:fill-sky-800 mt-2'>
                 {/* Likes */}
                 <div className='flex flex-row items-center mr-2'>
+                    {/* hasLiked ? ( */}
+                    <svg className='fill-red-500' xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z" /></svg>
+                    {/* ) : ( */}
+                    {/* <svg className='fill-red-500' xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z" /></svg> */}
+                    {/* ) */}
+                    {/* <svg className='mr-[2px]' xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="24"><path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z" /></svg> */ }
                     {/* <svg className='mr-1 text-1xl' xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M720-120H320v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h218q32 0 56 24t24 56v80q0 7-1.5 15t-4.5 15L794-168q-9 20-30 34t-44 14ZM240-640v520H80v-520h160Z" /></svg> */}
-                    <svg className='mr-[2px]' xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="24"><path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z" /></svg>
-                    <span className="text-base">
+                    <span className="ml-1 text-base">
                         {post.LikeCnt}
                     </span>
                 </div>
@@ -40,13 +45,13 @@ const PostCard: React.FC<PostCardProps> = ({ className, post }) => {
                 {/* Comments */}
                 <div className='flex flex-row items-center'>
                     <svg className="mr-[2px]" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="24"><path d="M240-400h480v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM880-80 720-240H160q-33 0-56.5-23.5T80-320v-480q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v720ZM160-320h594l46 45v-525H160v480Zm0 0v-480 480Z" /></svg>
-                    <span className="text-base">
+                    <span className="ml-1 text-base">
                         {post.CommentCnt}
                     </span>
                 </div>
 
                 {/* Categories */}
-                <div className='ml-auto text-sky-700 dark:text-sky-800 text-pretty text-sm md:text-lg'>
+                <div className='ml-auto text-sky-700 dark:text-sky-800 font-medium text-pretty text-sm md:text-lg'>
                     {
                         leafCategories && flattenOutCategoriesV1(leafCategories.find(category => category.Id === post.CategoryId))
                     }
