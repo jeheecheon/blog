f377f570e7811eb88317a4b0dc6aa526a493b043
Jehee Cheon <jeheecheon@gmail.com>
2024-02-26 12:49:40 +0900

    category and menu svg

--- Projects/fe/src/pages/blog/page/components/Header.tsx
+++ Projects/fe/src/pages/blog/page/components/Header.tsx
@@ -31,11 +31,16 @@ const Header: React.FC<HeaderProps> = () => {
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
     const location = useLocation();
-    const categoriesClass = useMemo(
+
+    const isOnCategory = useMemo(
         () => location.pathname.startsWith("/blog/categories/") || location.pathname.startsWith("/blog/recent-posts/pages/1")
+        , [location.pathname]
+    )
+    const categoriesClass = useMemo(
+        () => isOnCategory
             ? "text-orange-400"
             : "text-default-10-dark dark:text-default-7"
-        , [location.pathname]
+        , [isOnCategory]
     );
 
     useEffect(() => {
@@ -112,7 +117,9 @@ const Header: React.FC<HeaderProps> = () => {
                     onClick={() => setIsMenuOpen(!isMenuOpen)}
                 >
                     Menu &#160;
-                    <svg className="ml-3 w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400" viewBox="0 0 8 6" aria-hidden="true"><path d="M1.75 1.75 4 4.25l2.25-2.5" fill="none" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+                    <svg className={`stroke-default-10-dark dark:stroke-default-13-dark relative top-[2px] w-[13px] ${isMenuOpen && 'rotate-180'} transition-transform`} viewBox="0 0 8 6" aria-hidden="true"><path d="M1.75 1.75 4 4.25l2.25-2.5" fill="none" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+
+                    {/* <svg className="ml-3 w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400" viewBox="0 0 8 6" aria-hidden="true"><path d="M1.75 1.75 4 4.25l2.25-2.5" fill="none" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg> */}
                 </button>
                 <MenuModal isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
 
@@ -135,15 +142,12 @@ const Header: React.FC<HeaderProps> = () => {
                     <div>
                         <button
                             id="categories-button"
-                            className={`flex flex-row font-medium ${categoriesClass}`}
+                            className={`flex flex-row items-center font-medium ${categoriesClass}`}
                             onClick={() => setIsCategoryOpen(!isCategoryOpen)}
                         >
                             Categories &#160;
                             <p className="text-[9px]">
-                                {
-                                    isCategoryOpen ? '▲' : '▼'
-                                }
-
+                                <svg className={`${isOnCategory ? 'stroke-orange-400' : 'stroke-default-10-dark dark:stroke-default-7'} relative top-[2.5px] w-[13px] ${isCategoryOpen && 'rotate-180'} transition-transform`} viewBox="0 0 8 6" aria-hidden="true"><path d="M1.75 1.75 4 4.25l2.25-2.5" fill="none" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
                             </p>
                         </button>
                         {
@@ -223,7 +227,7 @@ const Header: React.FC<HeaderProps> = () => {
                     <svg className="w-7 fill-orange-300 dark:hidden stroke-orange-300" viewBox="0 0 24 24" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true"><path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z"></path><path d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061" fill="none"></path></svg>
                     <svg className="hidden w-7 fill-orange-300 stroke-orange-300 dark:block" viewBox="0 0 24 24" aria-hidden="true" ><path d="M17.25 16.22a6.937 6.937 0 0 1-9.47-9.47 7.451 7.451 0 1 0 9.47 9.47ZM12.75 7C17 7 17 2.75 17 2.75S17 7 21.25 7C17 7 17 11.25 17 11.25S17 7 12.75 7Z" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
                 </button>
-            </div>
+            </div >
         </aside >
     )
 };
