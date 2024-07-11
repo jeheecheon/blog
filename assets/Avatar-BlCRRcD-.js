9bf5182d9263ecb1f076984e64e7fae2e634f9a2
Jehee Cheon <jeheecheon@gmail.com>
2024-07-11 22:06:02 +0900

    Updates

--- assets/Avatar-BlCRRcD-.js
+++ assets/Avatar-BlCRRcD-.js
@@ -1,3 +1,3 @@
-import{j as e}from"./index--J1L9WHU.js";const l=({className:d,avatar:r,size:a=45,children:s})=>e.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?e.jsx("img",{src:r,className:`rounded-full
+import{j as e}from"./index-Cf5YIlTn.js";const l=({className:d,avatar:r,size:a=45,children:s})=>e.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?e.jsx("img",{src:r,className:`rounded-full
                             ${d} `,style:{width:`${a/16}rem`}}):s&&e.jsx("div",{className:`border-[0.0625rem] bg-default-9-dark dark:border-default-10-dark\r
                         ring-[0.156rem] ring-orange-300 rounded-full`,children:s})});export{l as A};
