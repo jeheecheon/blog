c30570429e9f7a698e287f41c964d53c46511f99
Jehee Cheon <jeheecheon@gmail.com>
2024-07-12 08:15:29 +0900

    Updates

--- assets/Avatar-fhtSZ7j1.js
+++ assets/Avatar-fhtSZ7j1.js
@@ -1,3 +1,3 @@
-import{j as e}from"./index-Knj8DWBX.js";const l=({className:d,avatar:r,size:a=45,children:s})=>e.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?e.jsx("img",{src:r,className:`rounded-full
+import{j as e}from"./index-qlEGfmU-.js";const l=({className:d,avatar:r,size:a=45,children:s})=>e.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?e.jsx("img",{src:r,className:`rounded-full
                             ${d} `,style:{width:`${a/16}rem`}}):s&&e.jsx("div",{className:`border-[0.0625rem] bg-default-9-dark dark:border-default-10-dark\r
                         ring-[0.156rem] ring-orange-300 rounded-full`,children:s})});export{l as A};
