0994407270f6a97a5e327f66e7e1c910dfeb00b9
Jehee Cheon <jeheecheon@gmail.com>
2024-07-20 02:40:44 +0900

    Updates

---
+++ assets/Avatar-kOnuVwtn.js
@@ -0,0 +1,3 @@
+import{j as e}from"./index-pftCXwDy.js";const l=({className:a,avatar:r,size:d=45,children:s})=>e.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?e.jsx("img",{src:r,alt:"author avatar",className:`rounded-full
+                            ${a} `,style:{width:`${d/16}rem`}}):s&&e.jsx("div",{className:`border-[0.0625rem] bg-default-9-dark dark:border-default-10-dark\r
+                        ring-[0.156rem] ring-orange-300 rounded-full`,children:s})});export{l as A};
--- assets/Avatar-qNZhJb9O.js
+++
@@ -1,3 +0,0 @@
-import{j as e}from"./index-3-xD8XpA.js";const l=({className:d,avatar:r,size:a=45,children:s})=>e.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?e.jsx("img",{src:r,className:`rounded-full
-                            ${d} `,style:{width:`${a/16}rem`}}):s&&e.jsx("div",{className:`border-[0.0625rem] bg-default-9-dark dark:border-default-10-dark\r
-                        ring-[0.156rem] ring-orange-300 rounded-full`,children:s})});export{l as A};
