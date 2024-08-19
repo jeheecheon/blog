174839bb08b1b9dbc66fea764b173df3a6fba0f4
jeheecheon <jeheecheon@gmail.com>
2024-08-20 01:04:55 +0900

    refactor: about 페이지 부컴포넌트화 진행

--- Projects/fe/src/app/page.tsx
+++ Projects/fe/src/app/page.tsx
@@ -35,40 +35,8 @@ const Blog = () => {
                     className="sm:w-1/2 sm:max-h-none sm:my-auto px-4 sm:px-0 pb-5 sm:pb-0 sm:pr-3
                     flex flex-col justify-center"
                 >
-                    <article
-                        className="text-[0.95rem] sm:text-base lg:text-lg font-[300] dark:font-extralight whitespace-pre-wrap text-pretty sm:leading-7
-                        text-default-18-dark dark:text-default-5"
-                    >
-                        <Blog.Introduction />
-                    </article>
-
-                    {/* Buttons */}
-                    <div className="space-x-4 mt-5">
-                        <a
-                            title="Github"
-                            target="_blank"
-                            href="https://github.com/jeheecheon"
-                            className="w-8 sm:w-9 inline-block"
-                        >
-                            <Github className="rounded-lg shadow-md shadow-gray-400/70 dark:shadow-none fill-gray-700 dark:fill-gray-800" />
-                        </a>
-
-                        <a
-                            title="Github"
-                            href="mailto:jeheecheon@gmail.com"
-                            className="w-8 sm:w-9 inline-block "
-                        >
-                            <Gmail className="rounded-lg shadow-md dark:shadow-none dark:text-stone-100" />
-                        </a>
-
-                        <a
-                            href="https://blog.jeheecheon.com/categories/recently-published"
-                            className="underline underline-offset-2 text-xs sm:text-sm text-orange-400 decoration-[0.01rem] font-normal sm:font-normal"
-                            target="_blank"
-                        >
-                            블로그 바로가기
-                        </a>
-                    </div>
+                    <Blog.Introduction />
+                    <Blog.Buttons />
                 </div>
             </div>
         </div>
@@ -77,7 +45,10 @@ const Blog = () => {
 
 Blog.Introduction = () => {
     return (
-        <>
+        <article
+            className="text-[0.95rem] sm:text-base lg:text-lg font-[300] dark:font-extralight whitespace-pre-wrap text-pretty sm:leading-7
+                        text-default-18-dark dark:text-default-5"
+        >
             <div className="flex text-lg sm:text-2xl lg:text-2xl font-medium">
                 <p>{`안녕하세요? `}</p>
                 <div className="animate-wave">👋</div>
@@ -98,7 +69,38 @@ Blog.Introduction = () => {
             <p>
                 웹쟁이 개발자로서, 웹에 관한 전반적인 지식을 공유하고 있습니다.
             </p>
-        </>
+        </article>
+    );
+};
+
+Blog.Buttons = () => {
+    return (
+        <div className="space-x-4 mt-5">
+            <a
+                title="Github"
+                target="_blank"
+                href="https://github.com/jeheecheon"
+                className="w-8 sm:w-9 inline-block"
+            >
+                <Github className="rounded-lg shadow-md shadow-gray-400/70 dark:shadow-none fill-gray-700 dark:fill-gray-800" />
+            </a>
+
+            <a
+                title="Github"
+                href="mailto:jeheecheon@gmail.com"
+                className="w-8 sm:w-9 inline-block "
+            >
+                <Gmail className="rounded-lg shadow-md dark:shadow-none dark:text-stone-100" />
+            </a>
+
+            <a
+                href="https://blog.jeheecheon.com/categories/recently-published"
+                className="underline underline-offset-2 text-xs sm:text-sm text-orange-400 decoration-[0.01rem] font-normal sm:font-normal"
+                target="_blank"
+            >
+                블로그 바로가기
+            </a>
+        </div>
     );
 };
 
