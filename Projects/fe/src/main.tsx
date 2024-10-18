9ac23d3bf141b0def710f9f628291e886094352d
jeheecheon <jeheecheon@gmail.com>
2024-10-19 07:17:39 +0900

    style<main>: 불필요한 fragment 태그 제거

--- Projects/fe/src/main.tsx
+++ Projects/fe/src/main.tsx
@@ -5,9 +5,7 @@ import App from "@/../App";
 import "@/_assets/css/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <>
-        <StrictMode>
-            <App />
-        </StrictMode>
-    </>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
